671df719d581801dfc8de739c4a2a08bda8185f0
pimnipaaa <101464821+pimnipaaa@users.noreply.github.com>
2023-03-17 09:56:14 +0700

    Add files via upload
    7hand

---
+++ GO-API/orm/Booking.go
@@ -0,0 +1,14 @@
+package orm
+
+import (
+"time"
+
+"gorm.io/gorm" // framwork ต่อกับ database ภาษา GO
+)
+type Booking struct { // สร้าง ตารางใน database ชื่อ User
+gorm.Model
+UserID string
+CarID string
+Start time.Time
+End time.Time
+}
